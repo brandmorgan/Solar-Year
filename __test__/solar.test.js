a115905e316de4a6bc800ffcba66273eaa03364c
brandmorgan <brandmorgan@gmail.com>
2023-04-14 15:21:39 -0700

    run first test in red phase

---
+++ __test__/solar.test.js
@@ -0,0 +1,35 @@
+import Conversions from "../src/solar.js";
+
+
+// class SolarConversions {
+//     constructor(age)
+//     this.age = age;
+// }
+
+// mercuryAge(){
+//     return this.age / 0.24;
+// }
+
+// venusAge() {
+//     return this.age / 0.62;
+// }
+
+
+//describe('SolarConversions', () =>{
+  //  test('return inpu age in mercurian years', () => {
+        
+       //parseExpectedArgs(this.age.mercuryAge).toEqual()
+
+  ////  })
+
+//}/)
+
+describe("Conversions", () => {
+  test("Returns age in Mercury years", () => {
+    const erik = new Conversions (30);
+    expect(this.age).toEqual(30);
+    });
+  });
+  
+  //   const mercuryAge = erik.age / 0.24;
+// expect(calculateMercuryAge(age)).toEqual(mercuryAge);
---
+++ src/solar.js
@@ -0,0 +1,8 @@
+//conversions
+
+export default class Conversions {
+    constructor (earthAge) {
+    this.earth = earthAge;
+    }
+};
+
